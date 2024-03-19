d2de95693cb18f5a5c84de83b19221eb71204ebf
boole <samarthupadhya2309@gmail.com>
2024-03-18 21:20:23 -0400

    multi key ordered statistic tree

---
+++ content/data-structures/MultiOST.h
@@ -0,0 +1,40 @@
+/**
+ * Author: Samarth Upadhya
+ * Date: 2024-03-15
+ * Description: Ordered Statistic Tree that can store multiple elements at the same time
+ * Time: O(log(n)) key order, O(log(n)) insert and erase
+ * Status: Tested
+ */
+
+#include <bits/extc++.h>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+struct MultiOST {
+    typedef pair<int, unsigned> pii; // pair.first holds the value of the element
+    typedef tree<
+        pii,
+        null_type,
+        less<pii>,
+        rb_tree_tag,
+        tree_order_statistics_node_update
+    > ost;
+
+    ost s;
+    unsigned cnt = 0;
+
+    MultiOST() = default;
+    MultiOST(initializer_list<int> l) { for(int x : l) insert(x); }
+    void insert(int x) { s.insert({x, cnt++}); }
+    ost::iterator find_by_order(int k) { return s.find_by_order(k); }
+    int order_of_key(int k) { return s.order_of_key({k, 0}); }
+    void erase(int x) {
+        auto it = s.lower_bound({x, 0});
+        // erase(it); // erases only first element
+        while(it != s.end() && it->first == x) erase(it++); // erases all elements
+    }
+    void erase(ost::iterator it) { s.erase(it); }
+    size_t size() const { return s.size(); }
+};
+
