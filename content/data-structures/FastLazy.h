f668e4b026702b99bab062e397ac1b776c692d3b
boole <samarthupadhya2309@gmail.com>
2024-05-09 00:30:33 -0400

    ACd https://www.spoj.com/problems/HORRIBLE/

--- content/data-structures/FastLazy.h
+++ content/data-structures/FastLazy.h
@@ -3,7 +3,7 @@
  * Date: 2024-05-08
  * Description: Fast array-based bottom-up segment tree for cheesers. 0-indexed and intervals are [inclusive, inclusive].
  * Time: O(log(n)) updates; O(log^2(n)) queries
- * Status: Not tested
+ * Status: Tested
  */
 
 struct segtree {
